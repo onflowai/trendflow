4ad31b209140357b757a92011123d71e22412ac1
dladden <dladden77@gmail.com>
2023-12-17 12:12:00 -0500

    local data retrieval test

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
+
+//test data for local storage
+let trends = [
+  { id: nanoid(), trend: 'chatgpt', type: 'language model' },
+  { id: nanoid(), trend: 'react', type: 'javascript framework' },
+];
+
 //setting up access to .env
 dotenv.config();
 //evoking app
 const app = express();
-
 //if we are in dev log the data if not do not
 if (process.env.NODE_ENV === 'development') {
   //HTTP request LOGGER middleware for Node calling dev
@@ -24,6 +31,12 @@ app.post('/', (req, res) => {
   console.log(req);
   res.json({ message: 'data received', data: req.body });
 });
+//setting up a GET to retrieve/read data in a route /api/v1/trends
+app.get('/api/v1/trends', (req, res) => {
+  //if there is anything but response 200 resource is not found
+  res.status(200).json({ trends }); //response fot the client
+});
+
 //this is done so that the hosting platform can inject the any value into the port with env
 const port = process.env.PORT || 5100;
 
