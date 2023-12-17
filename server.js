186509ac72df3a9be07991378287e3f1c68782cb
dladden <dladden77@gmail.com>
2023-12-17 14:24:59 -0500

    edit trend

--- server.js
+++ server.js
@@ -31,13 +31,14 @@ app.post('/', (req, res) => {
   console.log(req);
   res.json({ message: 'data received', data: req.body });
 });
-//setting up a GET to retrieve/read all trends in a route /api/v1/trends
+
+//GET a trend (setting up a retrieve/read all trends in a route /api/v1/trends)
 app.get('/api/v1/trends', (req, res) => {
   //if there is anything but response 200 resource is not found
   res.status(200).json({ trends }); //response fot the client
 });
 
-//adding a trend
+//ADD a trend
 app.post('/api/v1/trends', (req, res) => {
   //retrieving the trend and type from client
   const { trend, type } = req.body;
@@ -49,8 +50,44 @@ app.post('/api/v1/trends', (req, res) => {
   const id = nanoid(10); //nanoid generated 10 character id
   const trendObject = { id, trend, type }; //object with added trends
   trends.push(trendObject); //added trends pushed to the stack of all trends
+  res.status(201).json({ trendObject }); //response fot the client with created trend is 201
+});
 
-  res.status(200).json({ trendObject }); //response fot the client with created trend
+//GET SINGLE trend
+app.get('/api/v1/trends/:id', (req, res) => {
+  //retrieving the id
+  const { id } = req.params;
+  //retrieve the trend if it equals the id in the data
+  const trendObject = trends.find((trend) => trend.id === id);
+  //if the trend does not exist
+  if (!trendObject) {
+    return res.status(404).json({ msg: `no trend found with id ${id}` });
+  }
+  res.status(200).json({ trendObject }); //returning the found trend
+});
+
+//EDIT trend
+app.patch('/api/v1/trends/:id', (req, res) => {
+  //1: find trend
+  const { trend, type } = req.body;
+  //checking if trend and type
+  if (!trend || !type) {
+    res.status(400).json({ msg: 'please provide a trend' });
+    return;
+  }
+  //2: check if there is id
+  const { id } = req.params;
+  //retrieve the trend if it equals the id in the data
+  const trendObject = trends.find((trend) => trend.id === id);
+  //if the trend does not exist
+  if (!trendObject) {
+    return res.status(404).json({ msg: `no trend found with id ${id}` });
+  }
+  //3: modify the trend
+  trendObject.trend = trend;
+  trendObject.type = type;
+  //4: response
+  res.status(200).json({ msg: 'trend modified', trendObject }); //returning the found trend
 });
 
 //this is done so that the hosting platform can inject the any value into the port with env
