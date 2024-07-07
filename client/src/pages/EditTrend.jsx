0c17e8d7a12f7c3336da5d2d9d94d0c01fa104ad
dladden <dladden77@gmail.com>
2024-07-07 13:00:09 -0400

    setting up edit page

--- client/src/pages/EditTrend.jsx
+++ client/src/pages/EditTrend.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  FormComponent,
   FormSelector,
+  FallbackChart,
+  FormComponent,
   CustomErrorToast,
   CustomSuccessToast,
 } from '../components';
-import Container from '../assets/wrappers/DashboardFormContainer';
+import { RiEdit2Fill } from 'react-icons/ri';
+import { useUser } from '../context/UserContext'; // importing UserContext
+import { useOutletContext } from 'react-router-dom';
+import { useDashboardContext } from './DashboardLayout';
+import Container from '../assets/wrappers/EditTrendContainer';
+import { PiHashDuotone, PiEyeLight, PiTrendUp } from 'react-icons/pi';
+// import Container from '../assets/wrappers/TrendPageContainer';
 import { TREND_CATEGORY, TECHNOLOGIES } from '../../../utils/constants';
 import { Form, redirect, useLoaderData, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -18,13 +25,18 @@ import customFetch from '../utils/customFetch';
 //Fetching the trend data
 export const loader = async ({ params }) => {
   try {
-    const user = JSON.parse(localStorage.getItem('user')); // user info is stored in local storage
-    if (!user || user.role !== 'admin') {
-      toast.error('Unauthorized access!');
+    const { data } = await customFetch.get('/users/current-user'); // Fetch current user
+    if (!data.user || data.user.role !== 'admin') {
+      console.log(data.user);
+      toast.error(
+        <CustomErrorToast message="Unauthorized access to this Resource!" />
+      );
       return redirect('/dashboard');
     }
-    const { data } = await customFetch.get(`/trends/edit/${params.slug}`);
-    return data;
+    console.log('data :', data);
+    const trendResponse = await customFetch.get(`/trends/edit/${params.slug}`);
+    console.log('data2 :', trendResponse.data);
+    return { user: data.user, trendObject: trendResponse.data.trendObject };
   } catch (error) {
     toast.error(<CustomErrorToast message={error?.response?.data?.msg} />);
     return redirect('/dashboard');
@@ -62,38 +74,87 @@ const EditTrend = () => {
 
   return (
     <Container>
-      <Form method="post" className="form">
-        <h4 className="form-title">Edit Trend:</h4>
-        <div className="form-center">
-          <FormComponent
-            type="text"
-            name="trend"
-            defaultValue={trendObject.trend}
-          />
-          <FormSelector
-            labelText="Category"
-            name="trendCategory"
-            defaultValue={trendObject.trendCategory}
-            list={Object.values(TREND_CATEGORY)}
-          />
-          <FormSelector
-            labelText="Technology"
-            name="trendTech"
-            defaultValue={trendObject.trendTech}
-            list={Object.values(TECHNOLOGIES)}
-          />
-          <button
-            type="submit"
-            className="btn btn-block from-btn"
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? 'submitting...' : 'submit'}
-          </button>
-          <button type="button" onClick={handleDelete} className="btn info-btn">
-            Delete
-          </button>
+      <div className="trend-page-container">
+        <div className="page-layout">
+          <div id="section1" className="trend">
+            <Form method="post" className="">
+              <div className="form-center">
+                <div className="edit-trend-content">
+                  <div className="edit-trend">
+                    <RiEdit2Fill className="trend-edit-icon" />
+                    <FormComponent
+                      type="text"
+                      defaultValue={trendObject.trend}
+                    />
+                  </div>
+                  <div className="dummy-chart-controls">
+                    <div className="date-selector">
+                      <div className="circle"></div>
+                      {new Date().getFullYear() - 1}
+                      <div className="circle"></div>
+                      {new Date().getFullYear()}
+                      <div className="circle"></div>
+                      <div>
+                        <h5>Forecast</h5>
+                      </div>
+                      <FormSelector
+                        name="timePeriod"
+                        defaultValue="Select"
+                        list={['Daily', 'Weekly', 'Monthly']}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <FallbackChart />
+                <div className="content">
+                  <div className="content-selectors">
+                    <div className="selector">
+                      <RiEdit2Fill className="edit-icon" />
+                      <FormSelector
+                        name="trendCategory"
+                        defaultValue={trendObject.trendCategory}
+                        list={Object.values(TREND_CATEGORY)}
+                      />
+                    </div>
+                    <div className="selector">
+                      <RiEdit2Fill className="edit-icon" />
+                      <FormSelector
+                        name="trendTech"
+                        defaultValue={trendObject.trendTech}
+                        list={Object.values(TECHNOLOGIES)}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-actions">
+                    <div className="submit-btn">
+                      <button
+                        type="submit"
+                        className="btn btn-block from-btn"
+                        disabled={isSubmitting}
+                        style={{ marginLeft: 'auto' }} // Ensure button is glued to the right
+                      >
+                        {isSubmitting ? 'submitting...' : 'submit'}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="form-actions">
+                  <div className="delete-btn">
+                    <button
+                      type="button"
+                      onClick={handleDelete}
+                      className="btn info-btn"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </Form>
+            <div id="section2"></div>
+          </div>
         </div>
-      </Form>
+      </div>
     </Container>
   );
 };
