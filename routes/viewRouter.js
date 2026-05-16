a3f6a97d053f514639eae34b4acfceb3522762d9
dladden <dladden77@gmail.com>
2026-05-16 19:49:21 -0400

    auth for view router

--- routes/viewRouter.js
+++ routes/viewRouter.js
@@ -4,11 +4,12 @@ import {
   getViewTrendPreview,
   getViewCategoryPreview,
 } from '../controllers/viewController.js';
+import { authenticateUser } from '../middleware/authMiddleware.js';
 
 const router = Router();
 
-router.get('/approved-view-flow', getApprovedViewFlow);
-router.get('/trend-preview/:slug', getViewTrendPreview);
-router.get('/category-preview/:categoryValue', getViewCategoryPreview);
+router.get('/approved-view-flow', authenticateUser, getApprovedViewFlow);
+router.get('/trend-preview/:slug', authenticateUser, getViewTrendPreview);
+router.get('/category-preview/:categoryValue', authenticateUser, getViewCategoryPreview);
 
 export default router;
