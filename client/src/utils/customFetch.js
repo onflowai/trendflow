3bf37248739823df36d8ce0826c674276d055324
dladden <dladden77@gmail.com>
2026-05-27 16:57:43 -0400

    custom fetch update

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const isServer = import.meta.env.SSR;
+const baseURL = isServer
+  ? import.meta.env.VITE_DEV_API_BASE_URL || 'http://localhost:5100/api/v1'
+  : '/api/v1';
+
 const customFetch = axios.create({
-  baseURL: '/api/v1',
+  baseURL,
 });
 
 export default customFetch;
