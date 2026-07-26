38340ec27b5747db42623bececbb06450df85d88
dladden <dladden77@gmail.com>
2026-07-26 16:19:07 -0400

    skills data

---
+++ utils/skillData.js
@@ -0,0 +1,25 @@
+export const FILE_KINDS = [
+  'reference',
+  'script',
+  'asset',
+  'template',
+];
+
+export const TECH_ROLES = [
+  'primary',
+  'supporting',
+  'optional',
+];
+
+export const CURSOR_MODES = [
+  'native',
+  'rules',
+  'memory',
+  'unsupported',
+];
+
+export const RISK_LEVELS = [
+  'low',
+  'medium',
+  'high',
+];
